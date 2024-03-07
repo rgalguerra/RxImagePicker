22eff5939ead1688831b13c1961de2a347913f1b
Richard Galguerra <richard@solutionsresource.com>
2024-03-07 16:10:18 +0800

    - Update gradle and kotlin version

--- library/src/main/java/com/mlsdev/rximagepicker/RxImagePicker.kt
+++ library/src/main/java/com/mlsdev/rximagepicker/RxImagePicker.kt
@@ -207,8 +207,8 @@ class RxImagePicker : Fragment() {
     }
 
     private fun checkPermission(): Boolean {
-        return if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        return if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT >= 33) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < 33) {
                 requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
             }
             false
